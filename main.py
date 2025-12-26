185bcc10869293b60cdc61f9527148bf74accf7e
bartekspita <bartekspitza@hotmail.com>
2025-12-26 20:21:47 +0100

    added start of decode instruction

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import struct
+
 OFFSET = 0x80200000
+R_MASKS = [
+    0b0000_0000_0000_0000_0000_0000_0111_1111, # Op code
+    0b0000_0000_0000_0000_0000_1111_1000_0000, # rd
+    0b0000_0000_0000_0000_0111_0000_0000_0000, # funct3
+    0b0000_0000_0000_1111_1000_0000_0000_0000, # rs1
+    0b0000_0001_1111_0000_0000_0000_0000_0000, # rs2
+    0b1111_1110_0000_0000_0000_0000_0000_0000, # imm
+]
+twelwe_imm= 0b1111_1111_1110_0000_0000_0000_0000_0000, # imm
+
 class RV64:
     def __init__(self):
         self.regs = [0] * 32
@@ -10,18 +22,49 @@ class RV64:
         instr = struct.unpack('<I', self.mem[addr:addr+4])[0]
         return instr
     
+    def sign_extend(self, value, bits):
+        sign_bit_mask = 1 << (bits-1)
+
+        if value & sign_bit_mask:
+            # Negative: fill upper bits with 1s
+            mask = (1 << bits) - 1
+            return value | (~mask & 0xFFFFFFFFFFFFFFFF)
+        else:
+            # Positive: just mask to size
+            return value & ((1 << bits) - 1)
+    
+    def decode2(self, instr, type):
+        if type not in ["I", "R", "B", "S", "U", "J"]:
+            raise Exception(f"type={type} not allowed when decoding")
+        
+        opcode = instr & 0b1111_111
+
+        if type == "I":
+            return {
+                'opcode': opcode,
+                'rd': (R_MASKS[1] & instr) >> 7, 
+                'func3': (R_MASKS[2] & instr) >> 12,
+                'rs1': (R_MASKS[3] & instr) >> 15,
+                'imm': self.sign_extend((twelwe_imm & instr) >> 20, 12)
+            }
+        
+        raise NotImplementedError(f"type={type} not implemented")
+
+    
+    def decode(self, instr):
+        opcode = instr & 0b1111_111
+
+        if opcode == 0b11: # Load (lb, lh etc)
+            return self.decode2(instr, "I")
+
     def step(self):
         self.pc += 4
 
-if __name__ == '__main__':
-    cpu = RV64()
-
-    # Load kernel elf
+def load_kernel(cpu):
     path_to_kernel = '/Users/Bartek/gitreps/os/kernel.elf'
     kf = open(path_to_kernel, 'rb')
     kernel_data = kf.read() # Byte array of the entire elf
     kf.close()
-    import struct
 
     # Where does the program header table start, even though
     # it seems its always directly after the header, i.e. 64
@@ -50,3 +93,15 @@ if __name__ == '__main__':
             cpu.mem[mem_offset:mem_offset+p_filesz] = kernel_data[p_offset:p_offset+p_filesz]
             # Zero out bss
             cpu.mem[mem_offset+p_filesz:mem_offset+p_memsz] = b'\x00' * (p_memsz - p_filesz)
+
+if __name__ == '__main__':
+    cpu = RV64()
+    load_kernel(cpu)
+
+    instr = cpu.fetch()
+    print(hex(instr))
+
+    cpu.step()
+    instr = cpu.fetch()
+    print(hex(instr))
+
